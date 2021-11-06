5bc1dd30b78ccd6e8502eb94f73a486accbf7b74
Robert Gabriel Flores <robertgabrielxd2@gmail.com>
2021-11-06 09:19:23 -0400

    Version 1 movimiento de cabeza

---
+++ PoligonoDeTiro/Assets/ControlesJugador.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class ControlesJugador : MonoBehaviour
+{
+    [SerializeField] Transform camara;
+    [SerializeField] float mousesensi = 1;
+    
+    float verticalrotation;
+    public GameObject centerCam;
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+    void Update()
+    {
+        transform.Rotate(Vector3.up * Input.GetAxisRaw("Mouse X") * mousesensi);
+        verticalrotation += Input.GetAxisRaw("Mouse Y") * mousesensi;
+        verticalrotation = Mathf.Clamp(verticalrotation, -90f, 90f);
+        camara.localEulerAngles = new Vector3(verticalrotation, 0, 0);
+        
+    }
+}
